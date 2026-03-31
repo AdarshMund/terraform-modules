06160b684eebe8024a6b3ba2dc4787e48494782a
AdarshMund <adarsh@gmail.com>
2026-03-31 19:34:50 +0000

    ec2 module

--- modules/ec2-instances/variables.tf
+++ modules/ec2-instances/variables.tf
@@ -1,4 +1,12 @@
 variable "ami" {}
 variable "instance_type" {}
-variable "subnet_id" {}
 
+variable "subnet_id" {}
+variable "security_group_ids" {}
+
+variable "associate_public_ip" {}
+
+variable "key_name" {}
+
+
+variable "instance_tags" {}
