8218a87918a1819e46a73d48d882788455dc1ac6
AdarshMund <adarsh@gmail.com>
2026-03-31 19:15:46 +0000

    vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,10 +1,139 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+
+    tags = var.vpc_name
 }
+
+# public subnet 
 
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.public_subnet_cidr
+  availability_zone = var.az_1
+  map_public_ip_on_launch = var.map_public_ip 
+
+  tags = var.public_subnet_tags
+
 }
 
+# private subnet
+
+resource "aws_subnet" "private" {
+
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.private_subnet_cidr
+  availability_zone = var.az_2
+  tags = var.private_subnet_tags
+}
+
+# internet gateway
+
+resource "aws_internet_gateway" "this" {
+
+  vpc_id = aws_vpc.this.id
+
+  tags = var.igw_tags
+}
+
+# Elastic IP
+
+resource "aws_eip" "nat" {
+
+  domain = "vpc"
+  tags = var.eip_tags
+}
+
+# NAT gateway 
+
+resource "aws_nat_gateway" "this" {
+
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public.id
+
+  depends_on = [aws_internet_gateway.this]
+
+  tags = var.nat_tags
+}
+
+# Public Route table
+
+resource "aws_route_table" "public" {
+
+  vpc_id = aws_vpc.this.id
+
+  tags = var.public_rt_tags
+
+}
+
+resource "aws_route" "public" {
+
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = var.internet_cidr
+  gateway_id = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# private Route table
+
+resource "aws_route_table" "private" {
+
+  vpc_id = aws_vpc.this.id
+
+  tags = var.private_rt_tags
+}
+
+resource "aws_route" "private" {
+
+  route_table_id = aws_route_table.private.id
+  destination_cidr_block = var.internet_cidr
+  nat_gateway_id = aws_nat_gateway.this.id
+}
+
+resource "aws_route_table_association" "private" {
+
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+
+}
+
+# Security Group
+
+resource "aws_security_group" "this" {
+
+  name = var.sg_name
+  vpc_id = aws_vpc.this.id
+  description = var.sg_description
+
+  ingress {
+
+    from_port = var.ingress_ssh.from_port
+    to_port     = var.ingress_ssh.to_port
+    protocol    = var.ingress_ssh.protocol
+    cidr_blocks = var.ingress_ssh.cidr_blocks
+
+  }
+
+  ingress {
+
+    from_port = var.ingress_http.from_port
+    to_port     = var.ingress_http.to_port
+    protocol    = var.ingress_http.protocol
+    cidr_blocks = var.ingress_http.cidr_blocks 
+
+  }
+
+  egress {
+    from_port   = var.egress_all.from_port
+    to_port     = var.egress_all.to_port
+    protocol    = var.egress_all.protocol
+    cidr_blocks = var.egress_all.cidr_blocks
+  }
+
+  tags = var.sg_tags
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,2 +1,34 @@
-variable "cidr_block" {}
+variable "vpc_cidr" {}
+variable "vpc_tags" {}
 
+variable "public_subnet_cidr" {}
+variable "private_subnet_cidr" {}
+
+variable "public_subnet_tags" {}
+variable "private_subnet_tags" {}
+
+variable "map_public_ip" {}
+variable "az_1" {}
+variable "az_2" {}
+variable "igw_tags" {}
+variable "eip_tags" {}
+variable "nat_tags" {}
+
+variable "public_rt_tags" {}
+variable "private_rt_tags" {}
+
+variable "internet_cidr" {}
+
+
+
+
+variable "sg_name" {}
+variable "sg_description" {}
+variable "sg_tags" {}
+
+
+variable "ingress_ssh" {}
+variable "ingress_http" {}
+
+
+variable "egress_all" {}
